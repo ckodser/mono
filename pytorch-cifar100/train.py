8ed2fef842341b62f5a4a7f6b2a41ebc44a7c7c4
arshia.soltani2 <arshia.soltani2@gmail.com>
2024-07-28 19:00:51 +0430

    prints

--- pytorch-cifar100/train.py
+++ pytorch-cifar100/train.py
@@ -168,6 +168,10 @@ def eval_training(epoch=0, tb=True):
 
     if args.mono:
         from sklearn.metrics import confusion_matrix
+        for name, param in model.named_parameters():
+            if "_part_b" in name:
+                print(f"{name}| mean: {param.mean().item()}, variance: {param.var().item()}")
+
         for i in range(len(activations)):
             print(f"Layer {i} |top5% ", end="")
             activation = activations[i]  # torch.concatinate(activations, dim=1)
@@ -207,7 +211,7 @@ def eval_training(epoch=0, tb=True):
             (tn, fp), (fn, tp) = confusion_matrix(modified_activation.to(torch.int).flatten().cpu(),
                                                   modified_prediction.to(torch.int).flatten().cpu())
             total_samples = tn + fp + fn + tp
-            print("tn:{:.4f}, fp:{:.4f}, fn:{:.4f}, tp:{:.4f}|".format(
+            print("tn:{:.4f}, fp:{:.4f}, fn:{:.4f}, tp:{:.4f}".format(
                 tn / total_samples,
                 fp / total_samples,
                 fn / total_samples,
